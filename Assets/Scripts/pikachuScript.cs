928b5dc7b04a82a564369fe58e7b6a437a74ad3a
DavoLarris <davo.larris@gmail.com>
2017-02-21 18:57:10 +0100

    Platform not working :(

--- Assets/Scripts/pikachuScript.cs
+++ Assets/Scripts/pikachuScript.cs
@@ -4,50 +4,69 @@ using UnityEngine;
 
 public class pikachuScript : MonoBehaviour {
     private Animator animator;
-    private int speedJump = 20, speedRun = 10;
-    public KeyCode keySpace, aKey, dKey, rightKey, leftKey;
     private bool isJumping;
+    private bool onTouch;
     private Rigidbody2D rigid;
+    private Vector3 toLerp;
 
     // Use this for initialization
     void Start ()
     {
         rigid = this.GetComponent<Rigidbody2D>();
         animator = this.GetComponent<Animator>();
-        this.rigid.transform.rotation = Quaternion.identity; //estabilizacion
-        this.rigid.gravityScale = 6;
-        isJumping = false;
+        onTouch = false;
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rigid.velocity = movement * speedRun;
-
-        if (rigid.velocity.x > 0)
+    // Update is called once per frame
+    void Update()
+    {
+        if (onTouch)
         {
-            rigid.transform.rotation = Quaternion.identity; //estabilizacion
-            animator.SetTrigger("onRight");
-        } else if(rigid.velocity.x < 0)
-        {
-            rigid.transform.rotation = Quaternion.identity; //estabilizacion
-            animator.SetTrigger("onLeft");
-        } else
-        {
-            rigid.transform.rotation = Quaternion.identity; //estabilizacion
-            animator.SetTrigger("relax");
+            Jump();
+            Move();
         }
     }
 
-    void OnCollisionEnter2D(Collision2D col)
+    private void OnMouseDown()
     {
-        if (col.gameObject.tag == "ground")
+        onTouch = true;
+        animator.SetTrigger("upJump");
+    }
+
+    private void Jump()
+    {
+        toLerp = new Vector3(rigid.transform.position.x, 2, 1);
+        if (isJumping && rigid.transform.position.y < 1 - 0.5f)
+        {
+            rigid.transform.position = Vector3.Lerp(rigid.transform.position, toLerp, 3 * Time.deltaTime);
+        }
+        else
         {
             isJumping = false;
+            rigid.gravityScale = 1;
+        }
+    }
+    private void Move()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rigid.transform.eulerAngles = new Vector2(0, 180);
+            rigid.transform.Translate(Vector3.left * 5f * Time.deltaTime);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            rigid.transform.eulerAngles = new Vector2(0, 0);
+            rigid.transform.Translate(Vector3.left * 5f * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "ground")
+        {
+            isJumping = true;
+            rigid.gravityScale = 0;
+            rigid.velocity = Vector3.zero;
         }
     }
 }
